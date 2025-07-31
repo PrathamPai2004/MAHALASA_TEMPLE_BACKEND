1b80bcba5e0055ec925367ceb966d99f35151005
Pratham_Pai <122979370+PrathamPai2004@users.noreply.github.com>
2025-07-31 08:55:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import {registerModel,Room,sevaModel,sevaAdminModel} from './models/registerMode
 import cookieParser from 'cookie-parser';
 import nodemailer from 'nodemailer';
 import 'dotenv/config';
+import axios from 'axois'
 // import jwt, { verify } from 'jsonwebtoken'
 // import nodemailer from 'nodemailer';
 // const User =
